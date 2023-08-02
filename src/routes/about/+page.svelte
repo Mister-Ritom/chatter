de6bd16ad16338725f6d6d07ded5d2d948cb460d
Ritom <ritomghosh856@gmail.com>
2023-08-02 20:13:34 +0530

    Another fix

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -4,23 +4,9 @@
 </svelte:head>
 
 <div class="text-column">
-	<h1>About this app</h1>
-
-	<p>
-		This is a <a href="https://kit.svelte.dev">SvelteKit</a> app. You can make your own by typing the
-		following into your command line and following the prompts:
-	</p>
-
-	<pre>npm create svelte@latest</pre>
-
-	<p>
-		The page you're looking at is purely static HTML, with no client-side interactivity needed.
-		Because of that, we don't need to load any JavaScript. Try viewing the page's source, or opening
-		the devtools network panel and reloading.
-	</p>
-
-	<p>
-		The <a href="/sverdle">Sverdle</a> page illustrates SvelteKit's data loading and form handling. Try
-		using it with JavaScript disabled!
-	</p>
+	<div class="about">
+		<h1>RG-Chatter</h1>
+		<h2>Revolutionizing the Way You Connect</h2>
+		<p>Discover a new dimension of social interaction with RG-Chatter, a cutting-edge chat application designed to bring people together like never before. Much more than just a platform, RG-Chatter is a vibrant community where you can seamlessly connect with friends and like-minded individuals through servers and groups, all within an elegant and classy user interface.</p>
+	  </div>
 </div>
